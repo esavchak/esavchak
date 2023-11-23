46a04c2f000e0ef57bd747f43c9589a960bab77a
esavchak <151859198+esavchak@users.noreply.github.com>
2023-11-23 10:42:44 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/firefox2.jpg");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
